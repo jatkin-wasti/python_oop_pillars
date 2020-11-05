7c95bf89741b6e7e6278eaa4f895331f95e5d59a
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-05 11:39:42 +0000

    added functions and attributes to the Snake class, and showcased inheriting from Reptile and Animal

--- snake.py
+++ snake.py
@@ -1,7 +1,21 @@
 from reptile import Reptile
+
+
 # Creating a Snake class as a child class of Reptile
-
 class Snake(Reptile):
-
+    #  Initialising the Snake class, and using super() also use the initialise method in Reptile
     def __init__(self):
         super().__init__()
+        self.forked_tongue = True
+        self.venom = True
+
+    # Creating methods for our Snake class
+    def use_tongue(self):
+        return "Use your tongue to find prey"
+
+
+# nagini = Snake()  # Instantiating an object of the Snake class
+# print(nagini.venom)  # Has access to all of it's class functions and attributes
+# print(nagini.use_tongue())
+# print(nagini.cold_blooded)  # Also has access to attributes from Reptile, the parent class
+# print(nagini.breathe())  # And can ALSO access things from Animal, the Parent of Reptile
